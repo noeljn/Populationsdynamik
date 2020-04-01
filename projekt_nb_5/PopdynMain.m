b00b0ac1f9476f8dba207e07bb487f9ff730ad60
Tawsiful Islam <tawsiful11@hotmail.com>
2020-04-02 00:15:24 +0200

    Newtons done, main updated

--- projekt_nb_5/PopdynMain.m
+++ projekt_nb_5/PopdynMain.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 clear all
 format long
 
@@ -17,17 +16,3 @@ disp('V constant value:')
 disp(constants(1))
 disp('S constant value:')
 disp(constants(2))
-=======
-interpolation()    %Finding T1
-
-%Finding constant values for V and S and T2 after T1
-fun = @(V,S)[15.*V+1.7*10^(-5).*V.^2-0.022.*V.*S;
-             -1.9.*S.^(1.4)+0.088.*V.^(0.6).*S.^(0.8)];
-jac = @(V,S)[15-3.4*10^(-5).*V-0.022.*S,-0.022.*V;
-             0.0528.*S.^(0.8).*V.^(-0.4),-2.66.*S.^(0.4)+0.0704.*V.^(0.6).*S.^(-0.2)];
-roots=newtonsys(10^(-7),[880000;700],fun,jac);
-disp('V constant value:')
-disp(roots(1))
-disp('S constant value:')
-disp(roots(2))
->>>>>>> 4c477505fc197b078db1edbea31ae8d645388d51
--- projekt_nb_5/newtonsys.m
+++ projekt_nb_5/newtonsys.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function [roots]=newtonsys(tol,start,fun,jac)
     roots = start;
     h = 1;
@@ -10,25 +9,3 @@ function [roots]=newtonsys(tol,start,fun,jac)
         roots = roots + h;
     end
 end
-
-=======
-function [roots]=newtonsys(tol,start,fun,jac)
-
-fun = @(V,S)[15.*V-1.7*10^(-5).*V.^2-0.022.*V.*S;
-             -1.9.*S.^(1.4)+0.088.*V.^(0.6).*S.^(0.8)];
-jac = @(V,S)[15-3.4*10^(-5).*V-0.022.*S,-0.022.*V;
-             0.0528.*S.^(0.8).*V.^(-0.4),-2.66.*S.^(0.4)+0.0704.*V.^(0.6).*S.^(-0.2)];
-
-    roots = [880000;700];
-    tol = 10^(-7);
-    h = 1;
-    iter = 0;
-    while norm(h) > tol && iter < 5000
-        h=-jac(roots(1),roots(2))\fun(roots(1),roots(2));
-        roots = roots + h;
-        iter = iter+1;
-    end
-    disp('roots')
-    disp(roots)
-end
->>>>>>> 4c477505fc197b078db1edbea31ae8d645388d51
