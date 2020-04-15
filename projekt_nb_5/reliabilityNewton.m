2deead21e300cb1bb394bd60ae1feb225ca25569
Tawsiful Islam <tawsiful11@hotmail.com>
2020-04-15 20:13:43 +0200

    New reliability files

---
+++ projekt_nb_5/reliabilityNewton.m
@@ -0,0 +1,52 @@
+fun1 = @(u)[15.*u(1)-1.7*10^(-5).*u(1).^2-0.022.*u(1).*u(2);
+             -1.9.*u(2).^(1.4)+0.088.*u(1).^(0.6).*u(2).^(0.8)];
+jac1 = @(u)[15-3.4*10^(-5).*u(1)-0.022.*u(2),-0.022.*u(1);
+             0.0528.*u(2).^(0.8).*u(1).^(-0.4),-2.66.*u(2).^(0.4)+0.0704.*u(1).^(0.6).*u(2).^(-0.2)];
+
+fun2 = @(u)[15.*u(1)-1.7*10^(-5).*u(1).^2-0.022.*u(1).*u(2);
+             -1.9.*u(2).^(1.4)+0.088.*u(1).^(0.6).*u(2).^(0.8)-1.4.*u(2).*u(3);
+             -1.8.*u(3) + 0.028.*u(2).*sqrt(u(3))];
+jac2 = @(u)[15-3.4*10^(-5).*u(1)-0.022.*u(2),-0.022.*u(1),0;
+         0.0528.*u(2).^(0.8).*u(1).^(-0.4),-2.66.*u(2).^(0.4)+0.0704.*u(1).^(0.6).*u(2).^(-0.2)-1.4*u(3),-1.4.*u(2);
+         0,0.028.*sqrt(u(3)),-1.8 + 0.028.*u(2).*0.5.*1./sqrt(u(3))];
+
+for n = 1:2
+    order = -5;
+    if n ==1
+       disp('Running for model with V and S.')
+       fun = fun1;
+       jac = jac1;
+       start = [100000;700];
+    else
+        disp('Running for model with V, S and R.')
+        fun = fun2;
+        jac = jac2;
+        start = [100000;600;20];
+    end
+    roots = start;
+    tol = 10^(order);
+    for i = 1:3
+        
+        h = 1;
+        iter = 0;
+        hvec = [];
+        %Stops until tolerance is reached or iteration is 1000
+        while norm(h) > tol && iter < 1000
+            iter = iter+1;
+            h=-jac(roots)\fun(roots);
+            %disp(norm(h))
+            hvec = [hvec;norm(h)];
+            roots = roots + h;
+        end
+        fprintf('Tolerance order %d\n',order)
+        disp(hvec)
+        
+        if n ==1
+            fprintf('Constants are: V=%f, S=%f\n',roots(1),roots(2));
+        else
+            fprintf('Constants are: V=%f, S=%f, R=%f\n',roots(1),roots(2),roots(3));
+        end
+        order = order-1 ;
+        roots = start;
+    end
+end
---
+++ projekt_nb_5/reliabilityRK.m
@@ -0,0 +1,43 @@
+
+fun = @(v) 15*v - 1.7*10^(-5)*v^2;
+vmax = 15/(1.7*10^(-5));
+h=0.002; %Step length
+time=[];
+yvec = [];
+for i = 1:5
+    t = 0;
+    v = 100;
+    vvec = v;
+    tvec = t;
+    while not(v>vmax*0.99)
+        k1 = fun(v);
+        k2 = fun(v + h/2*k1);
+        k3 = fun(v + h/2*k2);
+        k4 = fun(v + h*k3);
+        v = v + h/6 * (k1 + 2*k2 + 2*k3 + k4);
+        t=t+h;
+        tvec =[tvec;t];
+        vvec = [vvec;v];        
+       
+        
+    end
+    [~,k] = min(abs(tvec-0.8));
+    yvec = [yvec;vvec(k)];
+    h = h/2;
+    
+    ymatrix = [tvec,vvec]';
+    [~,T1] = interpolT1(ymatrix,vmax*0.95);
+    time = [time;T1];
+end
+
+%Error in truncation will occur due to large numbers
+qvec = [];
+for i = 1:3
+    quotient = abs(yvec(i+1)-yvec(i))/abs(yvec(i+2)-yvec(i+1));
+    qvec = [qvec;quotient];
+end
+disp('Order of accuracy')
+disp(qvec)
+disp('The error in time is:')
+disp(abs(time(end)-time(1)))
+
